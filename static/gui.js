6eee1ee7c116e8e622bb4aaf5e45799df2ecb551
Hannes Mehnert <hannes@mehnert.org>
2014-01-31 23:03:58 +0000

    much more work on the gui part

--- static/gui.js
+++ static/gui.js
@@ -15,87 +15,173 @@ function toArray (xs) {
   return Array.prototype.slice.call(xs)
 }
 
-var shouldi = true
-
 function initialize () {
     var canvas = document.getElementById('canvas')
-    var graph = new Graph(canvas)
-    graph.layouter = new HierarchicLayouter(canvas.width, canvas.height)
-    canvas.onclick = function (event) {
+    var control = new Control()
+    var debug = document.getElementById("debug")
+    var cb = function (canvas, control, output, event) {
         var x = event.pageX - canvas.offsetLeft
         var y = event.pageY - canvas.offsetTop
-        var node = graph.findNodeAt(x, y)
-        graph.setselected(node)
+        var node = control.activeModel.getGraph().findNodeAt(x, y)
+        control.activeModel.getGraph().setselected(node)
+        output.innerHTML = "node is: " + node.value
     }
+    canvas.onclick = cb.curry(canvas, control, debug)
 
-    var phases = document.getElementById('phases')
-    var output = document.getElementById("output")
-    var debug = document.getElementById("debug")
-
-
-    var lastfun = ""
-    function handle_event (output, control, graph, event) {
+    function handle_event (output, control, forms, phases, event) {
         output.className = "normal"
         try {
             var val = event.data
             var res = eval(val)[0]
             var fun = "handle_" + res.type.replace(new RegExp("-", 'g'), "_")
-            lastfun = fun
-            var args = [graph, res]
-            if (res.nodeid != undefined)
-                args.push(graph.findNodeByID(res.nodeid))
-            if (res.other != undefined)
-                args.push(graph.findNodeByID(res.other))
-            if (res.old != undefined)
-                args.push(graph.findNodeByID(res.old))
-            control[fun].apply(control, args)
-            console.log("safely called " + fun)
+            control.lastfun = fun
+            if (fun == "handle_hello") {
+                control.resetModels()
+                clearElement(forms)
+                clearElement(phases)
+                console.log("successfully cleaned up")
+            } else {
+                if (res.formid) {
+                    var model = control.models[res.formid]
+                    if (model == undefined) {
+                        console.log("I create a new model now")
+                        var graph = new Graph(canvas)
+                        graph.layouter = new HierarchicLayouter(canvas.width, canvas.height)
+                        model = new Model(res.formid, graph)
+                        control.models[res.formid] = model
+                        var li = document.createElement("li")
+                        li.innerHTML = res.formid
+                        var cb = function (c, m) { c.setActiveModel(m) }
+                        li.onclick = cb.curry(control, model)
+                        forms.appendChild(li)
+                        if (control.activeModel == undefined)
+                            control.setActiveModel(model)
+                    }
+                    var graph = model.getGraph()
+                    var args = [graph, res]
+                    if (fun == "handle_optimizing")
+                        console.log("we want to break")
+                    if (res.nodeid != undefined)
+                        args.push(graph.findNodeByID(res.nodeid))
+                    if (res.other != undefined)
+                        args.push(graph.findNodeByID(res.other))
+                    if (res.old != undefined)
+                        args.push(graph.findNodeByID(res.old))
+                    control[fun].apply(control, args)
+                    console.log("safely called " + fun)
+                } else {
+                    var args = [res]
+                    control[fun].apply(control, args)
+                    console.log("safely called (without form!) " + fun)
+                }
+            }
         } catch (e) {
-            console.log("failed at or after " + lastfun)
+            console.log("failed at or after " + control.lastfun + " with " + e.message)
             output.className = "error"
             output.innerHTML = "error during " +  event + ": " + e
         }
     }
 
-    var control = new Control()
+
+    var output = document.getElementById("output")
+    var forms = document.getElementById("forms")
+    var phases = document.getElementById("phases")
 
     var evtSource = new EventSource("events")
-    evtSource.onmessage = handle_event.curry(output, control, graph)
+    evtSource.onmessage = handle_event.curry(output, control, forms, phases)
 
 
     var layout = document.getElementById("layout")
     layout.onclick = function () {
-        graph.layout()
-        graph.draw()
+        control.activeModel.getGraph().layout()
+        control.activeModel.getGraph().draw()
     }
 
 }
 
-function Control () {
+function Phase (name, graph) {
+    this.name = name
+    this.graph = graph
+}
+function Model (form, graph) {
+    this.formid = form
+    this.phases = [new Phase("initial", graph)]
+    this.activePhase = this.phases[0]
+}
+Model.prototype = {
+    getGraph: function () {
+        if ((this.activePhase == null) | (this.activePhase.graph == null))
+            console.log("undefined!!")
+        return this.activePhase.graph
+    },
+
+    setActivePhase: function (phase) {
+        this.activePhase = phase
+    },
+
+    addPhase: function (name, force) {
+        if (! this.getGraph().modified)
+            this.phases.pop
+        var graph = this.getGraph().copy()
+        var phase = new Phase(name, graph)
+        this.phases.push(phase)
+        this.setActivePhase(phase)
+    },
 }
 
+function Control () {
+    this.activeModel = null
+    this.models = null
+}
 Control.prototype = {
-    handle_hello: function (graph, json) {
-        //clearing graphs
-        graph.clear()
+    resetModels: function () {
+        this.models = new Object()
+        this.activemodel = null
+    },
+
+    setActiveModel: function (model) {
+        this.activeModel = model
+        //need to care about canvas stuff
+        var phaselist = document.getElementById("phases")
+        clearElement(phaselist)
+        for (var i = 0 ; i < model.phases.length ; i++) {
+            var ele = document.createElement("li")
+            ele.innerHTML = model.phases[i].name
+            var cb = function (model, phase) {
+                model.setActivePhase(phase)
+            }
+            ele.onclick = cb.curry(model, model.phases[i])
+            phaselist.appendChild(ele)
+        }
     },
 
     handle_new_computation: function (graph, json) {
         graph.insertNodeByID(json.nodeid, json.description[1])
     },
 
-    handle_add_temporary: function (graph, json, node, other) {
+    handle_new_temporary: function (graph, json, node, other) {
         var node = graph.insertNodeByID(json.nodeid, json.description[1])
         node.fillStyle = "lightblue"
         //it is the generator!
         var edge = graph.connect(other, node)
-        edge.strokeStyle = "lightblue"
+        if (edge)
+            edge.strokeStyle = "lightblue"
+    },
+
+    handle_new_object_reference: function (graph, json, node, other) {
+        var node = graph.insertNodeByID(json.nodeid, json.description[1])
+        node.fillStyle = "lightblue"
+        //it is the first user!
+        var edge = graph.connect(node, other)
+        if (edge)
+            edge.strokeStyle = "lightblue"
     },
 
     handle_next_computation_setter: function (graph, json, node, other, old) {
         graph.disconnect(node, old)
         var edge = graph.connect(node, other)
-        edge.strokeStyle = "black"
+        if (edge)
+            edge.strokeStyle = "black"
     },
 
     handle_remove_temporary: function (graph, json, node) {
@@ -112,27 +198,34 @@ Control.prototype = {
 
     handle_add_temporary_user: function (graph, json, node, other) {
         var edge = graph.connect(node, other)
-        edge.strokeStyle = "lightblue"
+        if (edge)
+            edge.strokeStyle = "lightblue"
     },
 
     handle_function_setter: function (graph, json, node, other, old) {
         graph.disconnect(old, node)
         var edge = graph.connect(other, node)
-        edge.strokeStyle = "lightblue"
+        if (edge)
+            edge.strokeStyle = "lightblue"
     },
 
     handle_generator_setter: function (graph, json, node, other, old) {
         graph.disconnect(old, node)
         var edge = graph.connect(other, node)
-        edge.strokeStyle = "lightblue"
+        if (edge)
+            edge.strokeStyle = "lightblue"
     },
 
-    handle_highlight_queue: function (graph, json) {
-        shouldi = false
+    handle_start_phase_for_code: function (graph, json) {
+        this.activeModel.addPhase(json.description, true)
+    },
+
+    handle_optimizing: function (graph, json, node) {
+        this.activeModel.addPhase(json.description + node.value)
     }
 }
 
-function clear_element (element) {
+function clearElement (element) {
     while (element.hasChildNodes())
         element.removeChild(element.firstChild)
 }
